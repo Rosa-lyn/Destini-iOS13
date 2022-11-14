a859efaddf420308341c092fb347275efe3efe01
Rosalyn Land <roz.s.land@gmail.com>
2022-11-14 14:11:37 +0000

    Move story logic into story brain

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
 
-    let stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
-                   Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-                   Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")]
-
-    var currentStory = 0
+    var storyBrain = StoryBrain()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,16 +23,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-        if sender.titleLabel?.text == "Take a left." {
-            currentStory = 1
-        } else {
-            currentStory = 2
-        }
+        guard let userChoice = sender.titleLabel?.text else { return }
+        storyBrain.nextStory(userChoice: userChoice)
 
         updateUI()
     }
 
     private func updateUI() {
+        let currentStory = storyBrain.currentStory
+        let stories = storyBrain.stories
+        
         storyLabel.text = stories[currentStory].title
         choice1Button.setTitle(stories[currentStory].choice1, for: .normal)
         choice2Button.setTitle(stories[currentStory].choice2, for: .normal)
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,18 @@
 
 import Foundation
 
+struct StoryBrain {
+    let stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+                   Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+                   Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")]
 
+    var currentStory = 0
+
+    mutating func nextStory(userChoice: String) {
+        if userChoice == "Take a left." {
+            currentStory = 1
+        } else if userChoice == "Take a right."{
+            currentStory = 2
+        }
+    }
+}
